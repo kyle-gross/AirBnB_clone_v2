5d60400cbb00c008fe6f9c3d500839d6b3036d57
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-11 18:31:00 +0000

    debug task 0 for deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # setup webstatic deployment
 
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 var="<html>\n  <head>\n  </head>\n  <body>\n\tHolberton School\n  </body>\n</html>"
 sudo mkdir /data/; sudo mkdir /data/web_static/
 sudo mkdir /data/web_static/releases/
@@ -11,3 +14,4 @@ sudo echo -e $var > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "s@^\tlocation / {@\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\n\tlocation / {@" /etc/nginx/sites-available/default
+exit 0
