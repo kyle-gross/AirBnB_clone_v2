afee8844547342023aa9d8ac55c56e9e4bb7c14d
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-11 22:31:55 +0000

    added task 3 and debuged 1 and 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,13 +2,14 @@
 """first fabric script"""
 
 
-from fabric.api import local, run, put
+from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """stuff"""
     time = datetime.now()
-    arch_name = "web_static" + time.strftime("%Y:%M:%d:%H:%M:%S") + ".tgz"
+    arch_name = "web_static_" + time.strftime("%Y%M%d%H%M%S") + ".tgz"
     v_path = "versions/" + arch_name
     tzg = "tar -cvzf " + v_path + " web_static"
     try:
