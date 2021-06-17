d1f6d205869f2135d4f6dcfc26572393621b6d98
Kyle <kyg555@outlook.com>
2021-06-17 23:24:27 +0000

    Add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module contains a script which starts Flask"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+hbnb.url_map.strict_slashes = False
+
+
+@hbnb.route('/')
+def func():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@hbnb.route('/hbnb')
+def func1():
+    """Displays 'HBNB'"""
+    return 'HBNB'
+
+
+@hbnb.route('/c/<text>')
+def func2(text):
+    """Displays 'C <text>'"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@hbnb.route('/python/<string:text>')
+@hbnb.route('/python/')
+def func3(text='is cool'):
+    """Displays 'Python <text>'"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@hbnb.route('number/<int:n>')
+def func4(n):
+    """Determines if <n> is a number"""
+    return '%i is a number' % n
+
+
+if __name__ == '__main__':
+    hbnb.run(debug=True, host='0.0.0.0')
