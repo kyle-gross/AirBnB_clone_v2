a72c2e5125978575ab20d7c4598abfb182ba66a5
Kyle <kyg555@outlook.com>
2021-06-17 23:05:50 +0000

    Add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains a script which starts Flask"""
+from flask import Flask
+
+hbnb = Flask(__name__)
+hbnb.url_map.strict_slashes = False
+
+
+@hbnb.route('/')
+def func():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@hbnb.route('/hbnb')
+def func1():
+    """Displays 'HBNB'"""
+    return 'HBNB'
+
+
+@hbnb.route('/c/<text>')
+def func2(text):
+    """Displays 'C <text>'"""
+    return 'C %s' % text
+
+if __name__ == '__main__':
+    hbnb.run(debug=True, host='0.0.0.0')
