26613d0e55bd030e2a03e1453388921897d11630
Kyle <kyg555@outlook.com>
2021-06-18 21:22:53 +0000

    Correct dictionary

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,13 +11,14 @@ app.url_map.strict_slashes = False
 @app.route('/states_list/')
 def states_list():
     state_dict = storage.all(State)
-    sorted_dict = sorted(new_dict.values(), key=lambda x: x.name)
-
+    sorted_dict = sorted(state_dict.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', State=sorted_dict)
+
 
 @app.teardown_appcontext
 def tear_down():
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
