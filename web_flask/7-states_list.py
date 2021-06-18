2f677c9a6a022bbbdd1969099119fbcdaf2d4b95
Kyle <kyg555@outlook.com>
2021-06-18 19:49:31 +0000

    Add several things

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """This script starts a Flask web application"""
 from flask import Flask
 from flask import render_template
+from models import storage, storage.all()
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,4 +10,7 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list/')
 def states_list():
-    return render_template('7-states_list.html')
+    return render_template('7-states_list.html', State=storage.all(State)[name].sort())
+
+if __name__ == '__main__':
+    hbnb.run(debug=True, host='0.0.0.0')
