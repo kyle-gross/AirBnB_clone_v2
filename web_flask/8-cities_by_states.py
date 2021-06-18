c677cf47b922b6731af3c8d7972b1f8d599d3043
Kyle <kyg555@outlook.com>
2021-06-18 21:58:40 +0000

    Add task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from models.city import City
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states/')
+def city_list():
+    # Create sorted State dict
+    state_dict = storage.all(State)
+    sorted_dict = sorted(state_dict.values(), key=lambda x: x.name)
+    # Create sorted City dict
+    city_dict = storage.all(City)
+    sorted_city = sorted(city_dict.values(), key=lambda x: x.name)
+    # Render template with 2 sorted dicts
+    return render_template('7-states_list.html', State=sorted_dict,
+                           City=sorted_city)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    if storage:
+        storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
