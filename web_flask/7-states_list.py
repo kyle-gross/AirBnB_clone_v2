b610d34a6e0c7fdfd901b36d87bb537487787985
Kyle <kyg555@outlook.com>
2021-06-18 21:13:06 +0000

    Sort dictionary in states_list func

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,25 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list/')
 def states_list():
+    # state_list = []
+    # state_dict = storage.all(State)
+    # for k, v in state_dict.items():
+    #     string = ""
+    #     if v == 'name', 'id':
+    #         string = state_dict[k] + ': ' + state_dict[v]
+    state_dict = storage.all(State)
+    new_dict = {}
+    for val in state_dict.values():
+        for k, v in val.items():
+            if k == 'name':
+                new_dict[k] = v
+            if k == 'id':
+                new_dict[k] = v
+    new_list = sorted(new_dict.items(), key=lambda x:x[1])
+    sort_dict = dict(new_list)
+
     return render_template('7-states_list.html',
-                           State=storage.all(State)[name].sort())
+                           State=sort_dict)
 
 
 if __name__ == '__main__':
