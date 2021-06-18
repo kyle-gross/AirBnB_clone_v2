900064e20f1e9beb21ffc93db8a6ada6575034be
Kyle <kyg555@outlook.com>
2021-06-18 19:54:42 +0000

    fix pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,9 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list/')
 def states_list():
-    return render_template('7-states_list.html', State=storage.all(State)[name].sort())
+    return render_template('7-states_list.html',
+                           State=storage.all(State)[name].sort())
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
