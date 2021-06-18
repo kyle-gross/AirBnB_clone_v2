beec40ff1e2adfb5db2017ac767ea22648e5ebdf
Kyle <kyg555@outlook.com>
2021-06-18 21:19:54 +0000

    Add teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,26 +10,14 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list/')
 def states_list():
-    # state_list = []
-    # state_dict = storage.all(State)
-    # for k, v in state_dict.items():
-    #     string = ""
-    #     if v == 'name', 'id':
-    #         string = state_dict[k] + ': ' + state_dict[v]
     state_dict = storage.all(State)
-    new_dict = {}
-    for val in state_dict.values():
-        for k, v in val.items():
-            if k == 'name':
-                new_dict[k] = v
-            if k == 'id':
-                new_dict[k] = v
-    new_list = sorted(new_dict.items(), key=lambda x:x[1])
-    sort_dict = dict(new_list)
+    sorted_dict = sorted(new_dict.values(), key=lambda x: x.name)
 
-    return render_template('7-states_list.html',
-                           State=sort_dict)
+    return render_template('7-states_list.html', State=sorted_dict)
 
+@app.teardown_appcontext
+def tear_down():
+    storage.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
