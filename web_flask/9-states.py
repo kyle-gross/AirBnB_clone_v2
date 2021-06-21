082f770cb4355d104e8e73c1b74f49f690165e12
Kyle <kyg555@outlook.com>
2021-06-21 21:32:02 +0000

    Add task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from models.city import City
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states/')
+@app.route('/states/<uuid:id>')
+def city_list(id=0):
+    new_dict = {}
+    # Create sorted State dict
+    state_dict = storage.all(State)
+    for state in state_dict.values():
+        if str(id) == state.id:
+            # Reduce to one state
+            new_dict['name'] = state.name
+            new_dict['id'] = state.id
+            break
+    if new_dict:
+        # Create sorted City dict
+        city_dict = storage.all(City)
+        sorted_city = sorted(city_dict.values(), key=lambda x: x.name)
+        # Render template with 2 sorted dicts
+        return render_template('9-states.html', state=new_dict,
+                               city=sorted_city, id=0)
+    else:
+        sorted_dict = sorted(state_dict.values(), key=lambda x: x.name)
+        return render_template('9-states.html', state=sorted_dict,
+                               city=None, id=1)
+        
+
+
+@app.teardown_appcontext
+def teardown(error):
+    if storage:
+        storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
